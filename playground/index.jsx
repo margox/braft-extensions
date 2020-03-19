03582e2788fcd31bb9e11a400e2c586b474e114f
韩硕 <18832643723@163.com>
2020-03-19 13:28:38 +0800

    header-id的importer方法增加textAlign和textIndent样式回显

--- playground/index.jsx
+++ playground/index.jsx
@@ -7,6 +7,7 @@ import BraftEditor from 'braft-editor'
 import Table from '../src/table'
 import ColorPicker from '../src/color-picker'
 import Markdown from '../src/markdown'
+import HeaderId from '../src/header-id'
 
 BraftEditor.use(Table({
   defaultColumns: 4,
@@ -20,8 +21,11 @@ BraftEditor.use(Markdown())
 
 BraftEditor.use(ColorPicker())
 
+BraftEditor.use(HeaderId());
+
 const tableStr = '<p></p><table border="1" style="border-collapse: collapse"><tr><td colSpan="1" rowSpan="1">ST-dstcollector	</td><td colSpan="1" rowSpan="1">80、12200</td></tr><tr><td colSpan="1" rowSpan="1">DST-dstweb		</td><td colSpan="1" rowSpan="1">80、83</td></tr></table><p></p>';
 const tableStr2 = '<table border="1" style="border-collapse: collapse"><colgroup><col width="120"></col><col width="240"></col></colgroup><tr><td colSpan="1" rowSpan="1">ST-dstcollector	</td><td colSpan="1" rowSpan="1">80、12200</td></tr><tr><td colSpan="1" rowSpan="1">DST-dstweb		</td><td colSpan="1" rowSpan="1">80、83</td></tr></table>';
+const tableStr3 = '<h1 id="efidd" style="text-align:left;text-indent:2em;"><strong><span style="font-size:0.64rem">优质物流服务协议</span></strong></h1>';
 
 class Demo extends React.Component {
 
@@ -30,8 +34,8 @@ class Demo extends React.Component {
     super(props)
 
     this.state = {
-      editorState: BraftEditor.createEditorState(tableStr2)
-    }
+      editorState: BraftEditor.createEditorState(tableStr3)
+    };
 
   }
 
